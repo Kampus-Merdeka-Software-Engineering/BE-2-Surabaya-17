f6e02897d1e9ef24f3452e1fd33948be654ce763
AlyaSalsabila <148780700+alyasls@users.noreply.github.com>
2023-12-10 15:09:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -9,23 +9,27 @@ const cors = require("cors");
 const app = express();
 
 const { PrismaClient } = require('@prisma/client');
+
 const prisma = new PrismaClient();
 
+const PORT = process.env.PORT || 3333;
 
+//middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
-
-
-
+//main route and respons json
 app.get("/", (req, res) => {
     res.json({ message: "Hello World." });
 });
 
+//require
 require('./routes/branches.routes')(app);
 require('./routes/specialty.routes')(app);
 require('./routes/reservation.routes')(app);
 
+//start server
 app.listen(3333, () => {
     console.log("Server is running on port 3333.");
 });
