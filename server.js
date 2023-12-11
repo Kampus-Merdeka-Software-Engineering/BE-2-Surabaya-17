450ac29a554bf8fe70538bad563cd65a1b78cd21
AlyaSalsabila <148780700+alyasls@users.noreply.github.com>
2023-12-11 10:10:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
 require('dotenv').config();
 
 const express = require("express");
-
 const bodyParser = require("body-parser");
-
 const cors = require("cors");
-
-const app = express();
-
 const { PrismaClient } = require('@prisma/client');
 
+const app = express();
 const prisma = new PrismaClient();
 
 const PORT = process.env.PORT || 3333;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//main route and respons json
+// Main route and response JSON
 app.get("/", (req, res) => {
     res.json({ message: "Hello World." });
 });
 
-//require
+// Require routes
 require('./routes/branches.routes')(app);
 require('./routes/specialty.routes')(app);
 require('./routes/reservation.routes')(app);
 
-//start server
-app.listen(3333, () => {
-    console.log("Server is running on port 3333.");
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
 });
