f88b4cf1e81f8d240e7542457e0c4093242195df
AlyaSalsabila <148780700+alyasls@users.noreply.github.com>
2023-12-11 11:50:04 +0700

    Create specialty2.controller.js

---
+++ controllers/specialty2.controller.js
@@ -0,0 +1,14 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+exports.getAll = async (req, res) => {
+  try {
+    const specialties = await prisma.specialties.findMany({
+      take: req.query.limit ? parseInt(req.query.limit) : 10,
+      skip: req.query.offset ? parseInt(req.query.offset) : 0,
+    });
+    res.send({ message: 'Data fetched successfully!', data: specialties });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
