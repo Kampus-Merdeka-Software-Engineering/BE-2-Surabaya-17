06ae8e7f03d59cd7341e92cd2b19f98398ee496b
AlyaSalsabila <148780700+alyasls@users.noreply.github.com>
2023-12-11 16:03:33 +0700

    Update reservation.controller.js

--- controllers/reservation.controller.js
+++ controllers/reservation.controller.js
@@ -2,17 +2,28 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-exports.createReservation = (req, res) => {
-    Reservation.create({
-        name: req.body.name,
-        phone: req.body.phone,
-        date: req.body.date,
-        time: req.body.time,
-        people: req.body.people,
-        branch: req.body.branch,
-        subject: req.body.subject,
-    }).then(async (newReservation) => {
-        let obj = {
+exports.createReservation = async (req, res) => {
+    try {
+        const { name, phone, date, time, people, branch, subject } = req.body;
+
+        // Validasi input sebelum membuat reservasi
+        if (!name || !phone || !date || !time || !people || !branch || !subject) {
+            return res.status(400).send({ message: "Semua bidang harus diisi" });
+        }
+
+        const newReservation = await prisma.reservation.create({
+            data: {
+                name,
+                phone,
+                date,
+                time,
+                people,
+                branch,
+                subject,
+            }
+        });
+
+        const obj = {
             name: newReservation.name,
             phone: newReservation.phone,
             date: newReservation.date,
@@ -20,10 +31,13 @@ exports.createReservation = (req, res) => {
             people: newReservation.people,
             branch: newReservation.branch,
             subject: newReservation.subject,
-        }
+        };
 
-        res.send({ message: "Reservation created successfully!", data: obj });
-    }).catch(err => {
-        res.status(500).send({ message: err.message });
-    });
-}
+        res.status(201).send({ message: "Reservasi berhasil dibuat!", data: obj });
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send({ message: "Terjadi kesalahan saat membuat reservasi" });
+    } finally {
+        await prisma.$disconnect();
+    }
+};
