98e18eb36bf4bd5b52201249d0bb12ab9ce10533
AlyaSalsabila <148780700+alyasls@users.noreply.github.com>
2023-12-11 12:57:57 +0700

    Update specialty2.controller.js

--- controllers/specialty2.controller.js
+++ controllers/specialty2.controller.js
@@ -1,28 +1,14 @@
-// Mengimpor PrismaClient dari modul @prisma/client
 const { PrismaClient } = require('@prisma/client');
-
-// Menginisialisasi PrismaClient untuk berinteraksi dengan database
 const prisma = new PrismaClient();
 
-// Ekspor fungsi getAllSpecialties2 yang dapat digunakan di tempat lain
-exports.getAllSpecialties2 = async (req, res) => {
+exports.getAll = async (req, res) => {
   try {
-    // Mengambil nilai limit dari query parameter jika ada, default: 10
-    const take = req.query.limit ? parseInt(req.query.limit) : 10;
-    
-    // Mengambil nilai offset dari query parameter jika ada, default: 0
-    const skip = req.query.offset ? parseInt(req.query.offset) : 0;
-
-    // Mengambil data spesialisasi dari database menggunakan Prisma
-    const specialties2 = await prisma.specialty2.findMany({ // Menggunakan "specialty2" bukan "specialties2"
-      take, // Jumlah data yang diambil
-      skip, // Jumlah data yang dilewati sebelum pengambilan data dimulai
+    const specialties = await prisma.specialty2.findMany({
+      take: req.query.limit ? parseInt(req.query.limit) : 10,
+      skip: req.query.offset ? parseInt(req.query.offset) : 0,
     });
-
-    // Mengirim respon berhasil dengan data yang berhasil diambil
-    res.send({ message: 'Data fetched successfully!', data: specialties2 });
+    res.send({ message: 'Data fetched successfully!', data: specialties });
   } catch (err) {
-    // Menangkap dan mengirim respon jika terjadi error
     res.status(500).send({ message: err.message });
   }
 };
